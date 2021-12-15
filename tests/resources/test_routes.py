08bbfb88ada0bd5e71ccece8622851040b9ba4cb
Steve Wangen <i.blue442@gmail.com>
2021-12-15 11:32:47 -0600

    linting and adding

--- tests/resources/test_routes.py
+++ tests/resources/test_routes.py
@@ -10,7 +10,7 @@ def test_read_main():
     assert response.json() == {"msg": "Hello World"}
 
 
-def test_read_verion():
+def test_read_version():
     response = client.get("/version")
     # pdb.set_trace()
     assert response.status_code == 200
