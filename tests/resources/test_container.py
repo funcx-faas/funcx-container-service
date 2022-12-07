ebef85ff044da791ee46a08dd0cebceb39a90a5d
Steven Wangen <i.blue442@gmail.com>
2022-12-07 16:59:05 -0600

    testing why test doesn't work in github...

--- tests/resources/test_container.py
+++ tests/resources/test_container.py
@@ -69,9 +69,10 @@ def test_delete_temp_dir(container_spec_fixture, settings_fixture):
                       settings_fixture,
                       temp_dir,
                       DOCKER_BASE_URL)
+        print(f'making sure {temp_dir} exists...')
 
         c.delete_temp_dir()
-        assert not os.path.exists(f'{temp_dir}')
+        assert not os.path.exists(temp_dir)
 
 
 @pytest.mark.skip(reason="having issues distinguishing tar vs gz - getting 'untar failed: truncated header' error")
